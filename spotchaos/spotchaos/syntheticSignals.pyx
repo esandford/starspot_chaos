f75c8009d6a9630bba4aa097cf7cf01f4e905c9e
Emily Sandford <esandford@astro.columbia.edu>
2024-09-01 11:35:04 +0200

    euclidean Cq analytic poisson  uncertainties following grassberger 1985

--- spotchaos/spotchaos/syntheticSignals.pyx
+++ spotchaos/spotchaos/syntheticSignals.pyx
@@ -932,7 +932,7 @@ def Cq(rArr, timeSeries, tau, m, theilerWindow=0):
             C2[rIdx] = (1./Npairs)*C2sum
             C2unc[rIdx] = np.sqrt( (1./(N - 1 - 2*theilerWindow)) * vArr[rIdx] )
     
-    return C0, C1, C2#, nArr
+    return C0, C1, C2, C0unc, C1unc, C2unc
 
 def d2_tisean(timeSeries,tau,mMax,rArr,thelier=0):
     """
